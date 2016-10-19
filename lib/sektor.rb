a389bb524eae9b12187e5e6f3edba2292f68ad0d
Luis Lopez <luigibyte@gmail.com>
2016-10-19 12:14:55 -0300

    improve require file logic by the use of a proxy parsers file, move parse logic to Sektor.parse method

--- lib/sektor.rb
+++ lib/sektor.rb
@@ -3,25 +3,17 @@ require "nokogiri"
 require "open-uri"
 require "net/http"
 require "sektor/track"
-require "sektor/parser/id"
-require "sektor/parser/title"
-require "sektor/parser/time"
-require "sektor/parser/artist"
-require "sektor/parser/size"
-require "sektor/parser/bitrate"
-require "sektor/parser/link"
-require "sektor/parser/track"
+require "sektor/parsers"
 
 module Sektor
   def self.search(title)
     url_encoded_title = CGI.escape(title)
     doc = Nokogiri::HTML(open("http://www.my-free-mp3.org/mp3/#{url_encoded_title}"))
-    result = Parser::Track.new(doc).parse
-
-    parse(result)
+    parse(doc)
   end
 
-  def self.parse(result)
+  def self.parse(doc)
+    result = Parser::Track.new(doc).parse
     result.map{ |r| Track.new(*r) }
   end
 end
---
+++ lib/sektor/parsers.rb
@@ -0,0 +1,9 @@
+require "sektor/parsers/id"
+require "sektor/parsers/title"
+require "sektor/parsers/time"
+require "sektor/parsers/artist"
+require "sektor/parsers/size"
+require "sektor/parsers/bitrate"
+require "sektor/parsers/link"
+require "sektor/parsers/track"
+
