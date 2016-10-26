67d0c0848bfc4ebfc20f3e740fa49caff88f8549
Luis Lopez <luigibyte@gmail.com>
2016-10-26 19:21:52 -0300

    test that Sektor.search returs kind of Sektor::Result

--- test/lib/sektor_test.rb
+++ test/lib/sektor_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class SektorTest < Minitest::Test
+
+  def setup
+    page_1 = File.read File.expand_path("page-1.html", "test/data")
+    page_2 = File.read File.expand_path("page-empty.html", "test/data")
+
+    stub_request(:get, "http://www.my-free-mp3.org/mp3/we+are+the+champions?page=1").
+      to_return(:status => 200, :body => page_1, :headers => {})
+
+    stub_request(:get, "http://www.my-free-mp3.org/mp3/we+are+the+champions?page=2").
+      to_return(:status => 200, :body => page_2, :headers => {})
+
+    stub_request(:post, "http://www.my-free-mp3.org/bitrate/").
+      to_return(:status => 200, :body => "<li>Size: 1.40 мб.&nbsp;&nbsp;&nbsp; Bitrate: 64 kbs.</li>", :headers => {})
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Sektor::VERSION
+  end
+
+  def test_that_search_returns_kind_of_result
+    assert_kind_of Sektor::Result, Sektor.search("we are the champions") 
   end
 end
 
